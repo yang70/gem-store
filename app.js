4b7e9c9beb4dbad7ba1a00b9a11099f9be8c69e7
Matthew Yang <yang70@gmail.com>
2015-10-11 08:49:09 -0700

    add image folder

--- app.js
+++ app.js
@@ -3,7 +3,19 @@
 
   app.controller('StoreController', function(){
     this.products = gems;
-  })
+  });
+
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
 
   var gems = [
     {
@@ -11,13 +23,22 @@
       price: 2.95,
       description: 'Some gems have hidden qualities beyond their luster, behond their shine... Dodeca is one of those gems.',
       canPurchase: true,
+      images: [
+        {
+          full: 'images/gem-01.gif'
+        }
+      ]
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: "Origin of the Pentagonal Gem is unknown, hence it's low value. It has a very high shine and 12 sides, however.",
       canPurchase: false,
+      images: [
+        {
+          full: 'images/gem-02.gif'
+        }
+      ]
     }
   ];
 })();
-

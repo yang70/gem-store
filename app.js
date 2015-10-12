ba761a585dd32bf99cf4ac929379097b241305b0
Matthew Yang <yang70@gmail.com>
2015-10-12 08:30:38 -0700

    finish chapter 4

--- app.js
+++ app.js
@@ -5,18 +5,6 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function(){
-    this.tab = 1;
-
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
-
   app.controller('ReviewController', function(){
     this.review = {};
 
@@ -24,6 +12,32 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function() {
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
     };
   });
 

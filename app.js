52d1bf68b32982bee477c54daa0dc7faf3a0a52f
Matthew Yang <yang70@gmail.com>
2015-10-12 13:08:27 -0700

    finish chapter 5, loading data from json, moved products directives into a module, including the panels controller

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 (function(){
-  var app = angular.module('store', []);
+  var app = angular.module('store', ['store-products']);
 
-  app.controller('StoreController', function(){
-    this.products = gems;
-  });
+  app.controller('StoreController', ['$http', function($http){
+    var store = this;
+
+    store.products = [];
+
+    $http.get('store-products.json').success(function(data){
+      store.products = data;
+    });
+  }]);
 
   app.controller('ReviewController', function(){
     this.review = {};
@@ -14,79 +20,4 @@
       this.review = {};
     };
   });
-
-  app.directive('productTitle', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-title.html'
-    };
-  });
-
-  app.directive('productPanels', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function() {
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-          this.tab = setTab;
-        };
-
-        this.isSelected = function(checkTab) {
-          return this.tab === checkTab;
-        };
-      },
-      controllerAs: 'panel'
-    };
-  });
-
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 2.95,
-      description: 'Some gems have hidden qualities beyond their luster, behond their shine... Dodeca is one of those gems.',
-      canPurchase: true,
-      images: [
-        {
-          full: 'images/gem-01.gif'
-        }
-      ],
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this product!",
-          author: "joe@thomas.com"
-        },
-        {
-          stars: 1,
-          body: "This product sucks",
-          author: "tim@hater.com"
-        }
-      ]
-    },
-    {
-      name: "Pentagonal Gem",
-      price: 5.95,
-      description: "Origin of the Pentagonal Gem is unknown, hence it's low value. It has a very high shine and 12 sides, however.",
-      canPurchase: false,
-      images: [
-        {
-          full: 'images/gem-02.gif'
-        }
-      ],
-      reviews: [
-        {
-          stars: 5,
-          body: "I love this product!",
-          author: "joe@thomas.com"
-        },
-        {
-          stars: 1,
-          body: "This product sucks",
-          author: "tim@hater.com"
-        }
-      ]
-    }
-  ];
 })();
